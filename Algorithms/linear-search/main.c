75e4a4ba95534d02f6055fe418c6084aa8a2ac59
i8DishantSingh <professional.id.dishant@gmail.com>
2026-04-06 22:02:06 +0530

    Implemented linear search algorithm

--- Algorithms/linear-search/main.c
+++ Algorithms/linear-search/main.c
@@ -1,5 +1,51 @@
 #include<stdio.h>
 
+int linearSearch(int arr[], int n, int target);
+void printArray(int arr[], int n);
+
+
 int main(){
     
+    int n, i, arr[100], target;
+
+    printf("Enter the number of elements in array: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the elements: \n");
+    for (i=0;i<n;i++){
+        printf("Arr[%d] = ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nEnter the targeted value: ");
+    scanf("%d", &target);
+
+    int index = linearSearch(arr, n, target);
+    printArray(arr, n);
+}
+
+int linearSearch(int arr[], int n, int target){
+
+    int i, j, foundValue;
+    printf("Starting linear Search...\n");
+    printf("Waiting...\n");
+    
+    for(i=0; i<n; i++){
+        if(arr[i] == target){
+            printf("Value found at index: %d\n", i);
+            foundValue = i;
+        }
+    }
+
+    printf("Linear Search Completed...");
+    return foundValue;
+
+}
+
+void printArray(int arr[], int n){
+
+    printf("Array: \n");
+    for(int i = 0; i<n; i++){
+        printf("Arr[%d] = %d\n", i, arr[i]);
+    }
 }
